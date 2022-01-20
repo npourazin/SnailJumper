4b00e60738e46c6df24db87b2d5aea20a402c3f9
Niki Pourazin <nikipourzin@gmail.com>
2022-01-20 09:31:03 +0330

    Implement NN feedforward

--- nn.py
+++ nn.py
@@ -10,7 +10,21 @@ class NeuralNetwork:
         :param layer_sizes: A list containing neuron numbers in each layers. For example [3, 10, 2] means that there are
         3 neurons in the input layer, 10 neurons in the hidden layer, and 2 neurons in the output layer.
         """
-        # TODO (Implement FCNNs architecture here)
+        # TODO (Choose best layer sizes)
+        self.layer_sizes = [3, 10, 2]  # input layer , hidden layer, output layer
+
+        # Implemented FCNNs architecture here
+        # There are 3 layers, between each two consecutive layers, there needs to be a weight matrix
+        self.weight = [
+            np.random.normal(size=(layer_sizes[1], layer_sizes[0])),  # weights between layer 0 and 1, aka W[0]
+            np.random.normal(size=(layer_sizes[2], layer_sizes[1]))  # weights between layer 1 and 2, aka W[1]
+        ]
+
+        # Initialize bias to 0, for every layer.
+        self.bias = [
+            np.zeros((layer_sizes[1], 1)),  # bias vector between layer 0 and 1, aka B[0]
+            np.zeros((layer_sizes[2], 1))  # bias vector between layer 1 and 2, aka B[1]
+        ]
         pass
 
     def activation(self, x):
@@ -19,8 +33,9 @@ class NeuralNetwork:
         :param x: Vector of a layer in our network.
         :return: Vector after applying activation function.
         """
-        # TODO (Implement activation function here)
-        pass
+
+        # Implemented activation function (sigmoid function) here.
+        return 1 / (1 + np.exp(-x))
 
     def forward(self, x):
         """
@@ -28,5 +43,18 @@ class NeuralNetwork:
         :param x: Input vector which is a numpy array.
         :return: Output vector
         """
-        # TODO (Implement forward function here)
-        pass
+
+        # Implemented the forward function here.
+        z = [
+            np.zeros((self.layer_sizes[0], 1)),
+            np.zeros((self.layer_sizes[1], 1)),
+            np.zeros((self.layer_sizes[2], 1))
+        ]
+
+        z[0] = x
+
+        for i in range(1, 3):
+            # for each next layer, z is calculated as shown below
+            z[i] = self.activation(self.weight[i - 1] @ z[i - 1] + self.bias[i - 1])
+
+        return z[2]
